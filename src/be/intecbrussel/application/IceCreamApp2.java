88ac6c4c708f64e63329c4b1b340d641608bb91a
savychev <werk3093059@gmail.com>
2025-06-13 15:01:46 +0200

    het voorkomen van NullPointerException

--- src/be/intecbrussel/application/IceCreamApp2.java
+++ src/be/intecbrussel/application/IceCreamApp2.java
@@ -24,6 +24,7 @@ public class IceCreamApp2 {
             };
         } catch (NoMoreIceCreamException e) {
             System.out.println(e.getMessage());
+            order = new Eatable[0];          // voorkomt NullPointerException
         }
 
         // alles opeten
