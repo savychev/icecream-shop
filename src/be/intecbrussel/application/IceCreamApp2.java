a25e2dae13144b0dbe48c26260d2413d967817cf
savychev <werk3093059@gmail.com>
2025-06-13 14:07:55 +0200

    IceCreamCar2 afwerken

--- src/be/intecbrussel/application/IceCreamApp2.java
+++ src/be/intecbrussel/application/IceCreamApp2.java
@@ -3,13 +3,7 @@ package be.intecbrussel.application;
 import be.intecbrussel.eatables.Cone;
 import be.intecbrussel.eatables.Eatable;
 import be.intecbrussel.eatables.Magnum;
-import be.intecbrussel.sellers.IceCreamCar;
-import be.intecbrussel.sellers.IceCreamSalon;
-import be.intecbrussel.sellers.PriceList;
-import be.intecbrussel.sellers.Stock;
-
-import java.util.ArrayList;
-import java.util.List;
+import be.intecbrussel.sellers.*;
 
 public class IceCreamApp2 {
     public static void main(String[] args) {
@@ -18,13 +12,26 @@ public class IceCreamApp2 {
         Stock stock = new Stock(5, 10, 1, 5);
         IceCreamCar car = new IceCreamCar(priceList, stock);
 
-        List<Eatable> order = new ArrayList<>(); // een array met datatype <Eatable> aanmaken (bevat list van instanties van classes die Eetable implimintert
-
+        Eatable[] order = null;
         try {
-
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+            // bestellingen plaatsen
+            order = new Eatable[]{
+                    car.orderIceRocket(),                                                                                 // 1.5
+                    car.orderIceRocket(),                                                                                 // 1.5
+                    car.orderCone(new Cone.Flavor[]{Cone.Flavor.STRAWBERRY, Cone.Flavor.LEMON}),                          // 1.5
+                    car.orderCone(new Cone.Flavor[]{Cone.Flavor.BANANA, Cone.Flavor.CHOCOLATE, Cone.Flavor.PISTACHE}),    // 2.25
+                    car.orderMagnum(Magnum.MagnumType.ALPINENUTS)                                                         // 3.75 == 2.5 (standard prijs) * 1.5 (kef)
+            };
+        } catch (NoMoreIceCreamException e) {
+            System.out.println(e.getMessage());
         }
+
+        // alles opeten
+        for (Eatable e : order) {
+            e.eat();
+        }
+
+        System.out.println("Totale profit: " + car.getProfit());
 
     }
 }
